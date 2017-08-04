ce98c249dbd3a15f599b64b8cbcf8eaefa9ec1f9
Tiancruyff <tiancruyff@163.com>
2017-08-04 15:12:32 +0800

    Add tinker

--- app/src/main/java/test/easemob/com/tian/MainActivity.java
+++ app/src/main/java/test/easemob/com/tian/MainActivity.java
@@ -3,11 +3,14 @@ package test.easemob.com.tian;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tinkerpatch.sdk.TinkerPatch;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TinkerPatch.with().fetchPatchUpdate(true);
 	}
 }
